bbc307fe208da66d3d98a264b4ee5bde03f3568c
BhDesai <BhDesai@BHDESAI-NB>
2023-10-14 12:28:04 +0530

    Removed System.out from file

--- src/main/java/com/dws/challenge/Thread/AccountThread.java
+++ src/main/java/com/dws/challenge/Thread/AccountThread.java
@@ -7,52 +7,56 @@ import java.util.concurrent.atomic.AtomicReference;
 import com.dws.challenge.domain.Account;
 import com.dws.challenge.service.NotificationService;
 
-public class AccountThread implements Runnable{
-	
+public class AccountThread implements Runnable {
+
 	private Account fromAccount;
 	private Account toAccount;
 	private AtomicReference<BigDecimal> amount;
 	private NotificationService notificationService;
-	
 
-	public AccountThread(Account fromAccount, Account toAccount, AtomicReference<BigDecimal> balance, NotificationService notificationService) {
-		
-		this.fromAccount=fromAccount;
-		this.toAccount=toAccount;
-		this.amount= balance;
-		this.notificationService=notificationService;
-		
+	public AccountThread(Account fromAccount, Account toAccount, AtomicReference<BigDecimal> balance,
+			NotificationService notificationService) {
+
+		this.fromAccount = fromAccount;
+		this.toAccount = toAccount;
+		this.amount = balance;
+		this.notificationService = notificationService;
+
 	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		//for(int i=0;i<100;i++) {
-			if (fromAccount.getAccountId().compareTo(toAccount.getAccountId()) < 0) {
-		          synchronized (fromAccount) {
-		              synchronized (toAccount) {
-		            	  System.out.println(" Thread --"+Thread.currentThread().getName());
-		            	  fromAccount.withdraw(amount.get());
-		            	  notificationService.notifyAboutTransfer(fromAccount, "The amount "+amount.get()+" is deducted from Account id "+ fromAccount.getAccountId()+" on "+LocalDateTime.now());
-		            	  toAccount.deposit(amount.get());
-		            	  notificationService.notifyAboutTransfer(toAccount, "The amount "+amount.get()+" is creadited to Account id "+ toAccount.getAccountId()+" on "+LocalDateTime.now());
-		            	
-		              }
-		          }
-		      } else {
-		          synchronized (toAccount) {
-		              synchronized (fromAccount) {
-		            	  System.out.println(" Thread --"+Thread.currentThread().getName());
-		            	  fromAccount.withdraw(amount.get());
-		            	  notificationService.notifyAboutTransfer(fromAccount, "The amount "+amount.get()+" is deducted from Account id "+ fromAccount.getAccountId()+" on "+LocalDateTime.now());
-			              toAccount.deposit(amount.get());
-			           	  notificationService.notifyAboutTransfer(toAccount, "The amount "+amount.get()+" is creadited to Account id "+ toAccount.getAccountId()+" on "+LocalDateTime.now());
-					         
-		              }
-		          }
-		      }
-		//}
+		// for(int i=0;i<100;i++) {
+		if (fromAccount.getAccountId().compareTo(toAccount.getAccountId()) < 0) {
+			synchronized (fromAccount) {
+				synchronized (toAccount) {
+					fromAccount.withdraw(amount.get());
+					notificationService.notifyAboutTransfer(fromAccount,
+							"The amount " + amount.get() + " is deducted from Account id " + fromAccount.getAccountId()
+									+ " on " + LocalDateTime.now());
+					toAccount.deposit(amount.get());
+
+					notificationService.notifyAboutTransfer(toAccount, "The amount " + amount.get()
+							+ " is creadited to Account id " + toAccount.getAccountId() + " on " + LocalDateTime.now());
+
+				}
+			}
+		} else {
+			synchronized (toAccount) {
+				synchronized (fromAccount) {
+					fromAccount.withdraw(amount.get());
+					notificationService.notifyAboutTransfer(fromAccount,
+							"The amount " + amount.get() + " is deducted from Account id " + fromAccount.getAccountId()
+									+ " on " + LocalDateTime.now());
+					toAccount.deposit(amount.get());
+					notificationService.notifyAboutTransfer(toAccount, "The amount " + amount.get()
+							+ " is creadited to Account id " + toAccount.getAccountId() + " on " + LocalDateTime.now());
+
+				}
+			}
+		}
+		// }
 	}
-	
 
 }
